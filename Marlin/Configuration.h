d604a04c7c6c74277633054f9378ab5cb33d99ad
stoto <stoto@stoto.net>
2017-04-22 15:04:13 +0200

    calibrate extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -486,7 +486,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 100.47095761381482 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 96.607 }
 
 /**
  * Default Max Feed Rate (mm/s)
